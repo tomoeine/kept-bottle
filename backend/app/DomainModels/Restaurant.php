aa9c0aa71346f10be066cc8fc6be365c94633fb4
Tomoe Sawai <tomoe.teshima@gmail.com>
2021-07-11 10:10:07 +0900

    [clean] Restaurant モデルのプロパティ初期化、PHPCS

--- backend/app/DomainModels/Restaurant.php
+++ backend/app/DomainModels/Restaurant.php
@@ -4,11 +4,6 @@ namespace App\DomainModels;
 
 class Restaurant
 {
-    public int $id;
-    public string $name;
-    public float $latitude;
-    public float $longitude;
-
     /**
      * Restaurant constructor.
      * @param int $id
@@ -16,11 +11,11 @@ class Restaurant
      * @param float $latitude
      * @param float $longitude
      */
-    public function __construct(int $id, string $name, float $latitude, float $longitude)
-    {
-        $this->id = $id;
-        $this->name = $name;
-        $this->latitude = $latitude;
-        $this->longitude = $longitude;
+    public function __construct(
+        public int $id,
+        public string $name,
+        public float $latitude,
+        public float $longitude
+    ) {
     }
 }
